24826c0760f8635f542cae9c4b0a53fed8f5df13
KunwarRajveerSingh <73644014+KunwarRajveerSingh@users.noreply.github.com>
2021-08-24 09:07:58 -0700

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,18 @@
+import csv
+with open('SOCR-HeightWeight.csv', newline='') as f:
+    reader = csv.reader(f)
+    filedata = list(reader)
+
+filedata.pop(0)
+newdata = []
+for i in range(len(filedata)):
+    num = filedata[i][1]
+    newdata.append(float(num))
+
+n = len(newdata)
+total = 0
+for x in newdata:
+    total += x
+
+mean = total / n
+print("mean / Averge is:- " + str(mean))
